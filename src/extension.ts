3b815a30d71ba93ac2303425357d111936517739
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-07-29 16:05:38 +0100

    basic commands and deactivate logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 import * as aura from "./aura_sdk";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let connected = false;
+
+
 export function activate(context: vscode.ExtensionContext) {
 
 	try {
 		aura.init();
+		connected = true;
 	} catch (e) {
 		console.error(e);
 		if (e instanceof Error) {
@@ -17,27 +17,37 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-aura" is now active!');
+	console.log("Congratulations, your extension \"vscode-aura\" is now active!");
 
 	aura.set_all_to_color(0);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-aura.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		aura.set_all_to_color(aura.rgb_to_color(255, 255, 255));
-		setTimeout(() => {
-			aura.set_all_to_color(0);
-		}, 1000);
-		vscode.window.showInformationMessage('Hello World from ASUS Aura Sync for VSCode!');
-	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vscode-aura.testAura", () => {
+			if (connected) {
+				aura.set_all_to_color(aura.rgb_to_color(255, 255, 255));
+				setTimeout(() => {
+					aura.set_all_to_color(0);
+				}, 1000);
+				vscode.window.showInformationMessage("Aura is connected!");
+			} else {
+				vscode.window.showInformationMessage("Aura is not connected!");
+			}
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vscode-aura.closeAura", () => {
+			aura.close();
+			connected = false;
+			vscode.window.showInformationMessage("Closed connection to Aura. Reload the extension to reconnect.");
+		})
+	);
+
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {
+	if (connected) {
+		aura.close();
+	}
+}
